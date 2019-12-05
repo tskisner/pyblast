d049adea63bde3b0ec98053b54bb72044cb83a67
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2019-12-05 10:22:41 -0600

    Udating distname

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ packagenames = find_packages()
 scripts = glob.glob(os.path.join('bin', '*'))
 
 PACKAGENAME = 'pyblast'
-DISTNAME = 'pyblast' #'YOUR DISTRIBTUION NAME (I.E. PIP INSTALL DISTNAME)' Generally good to be same as packagename
+DISTNAME = 'nublast' #'YOUR DISTRIBTUION NAME (I.E. PIP INSTALL DISTNAME)' Generally good to be same as packagename
 AUTHOR = 'Scott Coughlin'
 AUTHOR_EMAIL = 'scottcoughlin2014@u.northwestern.edu'
 LICENSE = 'GPLv3+'
